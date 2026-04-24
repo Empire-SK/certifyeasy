333d991e224ed580be0a8aae5e85b7d7e6b35e56
Empire <sabinksanthosh6@gmail.com>
2026-04-25 00:37:54 +0530

    chore: switch database provider to postgresql and remove sqlite migrations

--- prisma/migrations/20260424160655_add_issuer_profile/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "IssuerProfile" (
-    "id" TEXT NOT NULL PRIMARY KEY DEFAULT 'singleton',
-    "name" TEXT NOT NULL DEFAULT 'CertifyEasy Official',
-    "website" TEXT NOT NULL DEFAULT 'https://certifyeasy.com',
-    "email" TEXT NOT NULL DEFAULT 'verify@certifyeasy.com',
-    "address" TEXT NOT NULL DEFAULT '123 Tech Avenue, Silicon Valley, CA',
-    "description" TEXT NOT NULL DEFAULT 'A global leader in secure digital credentialing and verification services.'
-);
